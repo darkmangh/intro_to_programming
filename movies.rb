2802b14c3e34c6f8253334d86953e2e771179cb0
darkmangh <charles_rex@yahoo.com>
2016-02-16 01:45:49 +0000

    movies.rb

---
+++ movies.rb
@@ -0,0 +1,11 @@
+movies = { chucky: 1908,
+          charles: 1915,
+          yaw: 1997,
+          iron_man: 1998,
+          transformers: 1957,}
+
+puts movies[:charles]
+puts movies[:transformers]
+puts movies[:iron_man]
+puts movies[:yaw]
+puts movies[:chucky]
